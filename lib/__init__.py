4719be789bd341b3f06966986874b06507d17655
Robert Abel <rabel@robertabel.eu>
2021-12-23 16:49:30 +0100

    lib: add some plumbing logic for upcoming puzzles

---
+++ lib/__init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import sys
+
+from typing import Callable
+
+__all__ = [
+    'MultiLineFormatter',
+    'setup',
+]
+
+class MultiLineFormatter(logging.Formatter):
+    def format(self, record):
+        str = logging.Formatter.format(self, record)
+        header, footer = str.split(record.message)
+        str = str.replace('\n', '\n' + ' '*len(header))
+        return str
+
+def setup(install_arguments: Callable[[argparse.ArgumentParser], None] = None) -> argparse.Namespace:
+    # Set up Logger
+    l = logging.getLogger()
+    h = logging.StreamHandler()
+    h.setFormatter(MultiLineFormatter(fmt='[%(asctime)s][%(levelname)-8s] %(message)s', datefmt='%d %b %Y %H:%M:%S'))
+    l.addHandler(h)
+    l.setLevel(logging.INFO)
+
+    logLevelMap = {
+        'debug':   logging.DEBUG,
+        'info':    logging.INFO,
+        'warning': logging.WARNING,
+        'error':   logging.ERROR,
+        }
+    
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Advent of Code.', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--loglevel', help='Loglevel, one of \'DEBUG\', \'INFO\' (default), \'WARNING\', \'ERROR\'.', type=str, default='INFO')
+    parser.add_argument('--example', help='Use example data.', type=int, choices=range(1,10+1), default=0)
+    if (install_arguments is not None):
+        install_arguments(parser)
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    if (sys.version_info < (3, 8)):
+        logging.fatal(f'Python version {".".join(sys.version_info)} is less than minimum required version 3.8.')
+        sys.exit(-1)
+    
+    # Set User Loglevel
+    logLevel = logLevelMap.get(args.loglevel.lower(), None)
+    if (logLevel is None):
+        logging.error('Invalid loglevel \'{0:s}\' passed. Exiting...'.format(args.loglevel))
+        sys.exit(-1)
+    
+    l.setLevel(logLevel)
+    return args
