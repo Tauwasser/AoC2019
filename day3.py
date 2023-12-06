a492ddb048210f64fde1dffca9c51a134db14702
Robert Abel <rabel@robertabel.eu>
2023-12-06 21:10:31 +0100

    Day 3 Pt 2
    Signed-off-by: Robert Abel <rabel@robertabel.eu>

--- day3.py
+++ day3.py
@@ -22,14 +22,14 @@ example_input = """467..114..
 .664.598..
 """
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class Number:
     x: int
     y: int
     length: int
     value: int
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class Symbol:
     x: int
     y: int
@@ -41,6 +41,13 @@ class Schematic:
     height: int
     numbers: list[Number] = field(default_factory=list, repr=False)
     symbols: list[Symbol] = field(default_factory=list, repr=False)
+
+@dataclass(eq=True, frozen=True)
+class Gear:
+    x: int
+    y: int
+    lhs: Number
+    rhs: Number
 
 def read_inputs(example=0) -> Schematic:
     
@@ -55,7 +62,7 @@ def read_inputs(example=0) -> Schematic:
     line_length = data.find('\n') + 1
     num_lines = data.count('\n')
     
-    schematic = Schematic(line_length - 1, num_lines - 1)
+    schematic = Schematic(line_length - 1, num_lines)
     
     for match in pattern.finditer(data):
         
@@ -102,16 +109,47 @@ def part1(schematic: Schematic) -> List[Number]:
     
     return result
 
-def part2():
-    pass
+def part2(schematic: Schematic) -> list[Gear]:
+    
+    result : list[Gear] = []
+    X = schematic.width - 1
+    Y = schematic.height - 1
+    
+    number_map = {(xx, number.y): number for number in schematic.numbers for xx in range(number.x, number.x + number.length)}
+    possible_gears = list(filter(lambda symbol: symbol.symbol == '*', schematic.symbols))
+    
+    for symbol in possible_gears:
+        
+        # all possible positions
+        x = symbol.x
+        y = symbol.y
+        
+        # previous line
+        adjacent_pos = [(xx, y - 1) for xx in range(x - 1, x + 1 + 1) if y - 1 >= 0 and 0 <= xx <= X]
+        # next line
+        adjacent_pos += [(xx, y + 1) for xx in range(x - 1, x + 1 + 1) if y + 1 <= Y and 0 <= xx <= X]
+        # same line
+        adjacent_pos += [(x + xx, y) for xx in (-1, +1) if 0 <= x + xx <= X]
+        
+        # check if any two numbers are adjacent
+        adjacent_numbers = set(filter(None, map(number_map.get, adjacent_pos)))
+        
+        if (len(adjacent_numbers) != 2):
+            continue
+        
+        lhs = adjacent_numbers.pop()
+        rhs = adjacent_numbers.pop()
+        result.append(Gear(x, y, lhs, rhs))
+    
+    return result
 
 def main(args):
     
     schematic = read_inputs(args.example)
     part_numbers = part1(schematic)
     logging.info(f'Part 1: {sum(num.value for num in part_numbers)} ({", ".join(str(num.value) for num in part_numbers)})')
-    part2()
-    logging.info(f'Part 2: ')
+    gears = part2(schematic)
+    logging.info(f'Part 2: {sum(gear.lhs.value * gear.rhs.value for gear in gears)} ({", ".join(f"({gear.x}, {gear.y})" for gear in gears)})')
 
 if __name__ == '__main__':
     args = setup()
