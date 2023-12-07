4160fb5673493879ca6b6e920ed18095f06ae91a
Robert Abel <rabel@robertabel.eu>
2023-12-07 22:29:48 +0100

    Day 7 Pt 2
    Signed-off-by: Robert Abel <rabel@robertabel.eu>

--- day7.py
+++ day7.py
@@ -20,6 +20,7 @@ QQQJA 483
 """
 
 Card = enum.IntEnum('Card', '2 3 4 5 6 7 8 9 T J Q K A')
+CardV = enum.IntEnum('CardV', 'J 2 3 4 5 6 7 8 9 T Q K A')
 
 @classmethod
 def card_from_string(cls, value):
@@ -29,41 +30,18 @@ def card_from_string(cls, value):
     return obj
 
 Card.from_string = card_from_string
+CardV.from_string = card_from_string
 
 HandType = enum.IntEnum('HandType', 'HIGH PAIR_1 PAIR_2 KIND_3 FULL_HOUSE KIND_4 KIND_5')
 
 @dataclass(eq=True, frozen=True)
-class Hand:
+class HandBase:
     cards: tuple[Card]
     bid:   int
     
-    @functools.cached_property
-    def type(self):
-        
-        card_counts = defaultdict(lambda: 0)
-        
-        for card in self.cards:
-            card_counts[card] += 1
-        
-        match (tuple(sorted(card_counts.values(), reverse=True))):
-            case (5,):
-                return HandType.KIND_5
-            case (4, 1):
-                return HandType.KIND_4
-            case (3, 2):
-                return HandType.FULL_HOUSE
-            case (3, 1, 1):
-                return HandType.KIND_3
-            case (2, 2, 1):
-                return HandType.PAIR_2
-            case (2, 1, 1, 1):
-                return HandType.PAIR_1
-            case _:
-                return HandType.HIGH
-    
     def __repr__(self):
         """Repr w/ better readability and include type"""
-        return f'Hand(cards={"".join(c._name_ for c in self.cards)}, type={self.type._name_}, bid={self.bid})'
+        return f'{self.__class__.__name__}(cards={"".join(c._name_ for c in self.cards)}, type={self.type._name_}, bid={self.bid})'
     
     def __eq__(self, other):
         if self.__class__ is not other.__class__:
@@ -145,7 +123,67 @@ class Hand:
         # same type: all cards must be same too in same order
         return all(lhs == rhs for lhs, rhs in zip(self.cards, other.cards))
 
-def read_inputs(example=0):
+@dataclass(eq=True, frozen=True, repr=False)
+class Hand(HandBase):
+    
+    @functools.cached_property
+    def type(self):
+        
+        card_counts = defaultdict(lambda: 0)
+        
+        for card in self.cards:
+            card_counts[card] += 1
+        
+        match (tuple(sorted(card_counts.values(), reverse=True))):
+            case (5,):
+                return HandType.KIND_5
+            case (4, 1):
+                return HandType.KIND_4
+            case (3, 2):
+                return HandType.FULL_HOUSE
+            case (3, 1, 1):
+                return HandType.KIND_3
+            case (2, 2, 1):
+                return HandType.PAIR_2
+            case (2, 1, 1, 1):
+                return HandType.PAIR_1
+            case _:
+                return HandType.HIGH
+
+@dataclass(eq=True, frozen=True, repr=False)
+class HandV(HandBase):
+    
+    @functools.cached_property
+    def type(self):
+        
+        card_counts = defaultdict(lambda: 0)
+        
+        for card in self.cards:
+            card_counts[card] += 1
+        
+        if (CardV.J in card_counts and len(card_counts) > 1):
+            j_count = card_counts[CardV.J]
+            del card_counts[CardV.J]
+            argmax = max(card_counts, key=card_counts.get)
+            card_counts[argmax] += j_count
+        
+        match (tuple(sorted(card_counts.values(), reverse=True))):
+            case (5,):
+                return HandType.KIND_5
+            case (4, 1):
+                return HandType.KIND_4
+            case (3, 2):
+                return HandType.FULL_HOUSE
+            case (3, 1, 1):
+                return HandType.KIND_3
+            case (2, 2, 1):
+                return HandType.PAIR_2
+            case (2, 1, 1, 1):
+                return HandType.PAIR_1
+            case _:
+                return HandType.HIGH
+
+def read_inputs(example=0) -> tuple[list[Hand], list[HandV]]:
     
     match (example):
         case _ if (example):
@@ -156,13 +194,15 @@ def read_inputs(example=0):
     
     data = data.splitlines()
     hands : list[Hand] = []
+    handvs : list[HandV] = []
     
     for line in data:
         cards, bid = line.split(' ')
         
         hands.append(Hand(tuple(Card.from_string(c) for c in cards), int(bid)))
+        handvs.append(HandV(tuple(CardV.from_string(c) for c in cards), int(bid)))
     
-    return hands
+    return hands, handvs
 
 def part1(hands: list[Hand]) -> list[int]:
     """Compute ranks of Hands"""
@@ -174,11 +214,11 @@ def part2():
 
 def main(args):
     
-    hands = read_inputs(args.example)
+    hands, handvs = read_inputs(args.example)
     ranks = part1(hands)
     logging.info(f'Part 1: {sum(map(lambda r, h: r * h.bid, ranks, hands))} ({", ".join(str(rh) for rh in zip(ranks, hands))})')
-    part2()
-    logging.info(f'Part 2: ')
+    rankvs = part1(handvs)
+    logging.info(f'Part 2: {sum(map(lambda r, h: r * h.bid, rankvs, handvs))} ({", ".join(str(rh) for rh in zip(rankvs, handvs))})')
 
 if __name__ == '__main__':
     args = setup()
